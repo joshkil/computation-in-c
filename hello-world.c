1970aebebf89bd12b70dc51f6cd9cdd0b57e4596
Joshua Kilpatrick <kilpatrick.joshua@gmail.com>
2022-07-07 20:40:46 -0500

    Checkin Hello World

---
+++ hello-world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* A classic first program for any language. 
+ * Let's see how to print a message on the screen.
+*/
+
+int main(){
+    printf("Hello World!");
+    return 1;
+}
